303a8dfd245054899047df7fe8ab6f7347d457fa
Saheen Shoukath <saheenshoukath@gmail.com>
2024-12-03 22:32:05 +0530

    feat: add day two

---
+++ src/main/java/org/shellord/adventofcode/Day2.java
@@ -0,0 +1,59 @@
+package org.shellord.adventofcode;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day2 {
+    List<List<Integer>> parsedInput = parseInput();
+
+    public static void main(String[] args) {
+        Day2 day2 = new Day2();
+        System.out.println("Part One: " + day2.partOne());
+        System.out.println("Part Two: " + day2.partTwo());
+    }
+
+    public List<List<Integer>> parseInput(){
+      List<String> input =  Utils.readInputLines("Day2.input");
+      return input.stream().map(i-> Arrays.stream(i.split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList())
+        ).collect(Collectors.toList());
+    }
+
+    public long partOne(){
+       return parsedInput.stream().filter(this::isSafe).count();
+    }
+
+    private boolean isSafe(List<Integer> i){
+        boolean isIncreasing = false;
+        boolean isDecreasing = false;
+        for(int j = 0; j < i.size() -1 ; j++){
+            if(i.get(j) < i.get(j+1)){
+                isIncreasing = true;
+            }else{
+                isDecreasing = true;
+            }
+            if(isIncreasing && isDecreasing
+                    || Math.abs(i.get(j) - i.get(j+1)) > 3
+                    || Math.abs(i.get(j) - i.get(j+1)) < 1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public long partTwo(){
+        return parsedInput.stream().filter(i -> {
+          if(isSafe(i)){
+              return true;
+          }
+          for(int j = 0; j < i.size(); j++){
+              List<Integer> tempList = new ArrayList<>(i);
+              tempList.remove(j);
+              if(isSafe(tempList)){
+                  return true;
+              }
+          }
+          return false;
+        }).count();
+    }
+}
